7e2544433906db17f8828e09bd76ad5f30dbfa91
Egor Saveliev <esavelievCode@gmail.com>
2024-05-19 22:46:05 +0200

    update bootstrap-vcpkg

--- bootstrap-vcpkg.bat
+++ bootstrap-vcpkg.bat
@@ -1,4 +1,26 @@
 @echo off
-vcpkg/bootstrap-vcpkg.bat
-vcpkg/vcpkg.exe install curl jsoncpp
-vcpkg/vcpkg.exe integrate install
+setlocal
+
+REM Установить путь к vcpkg
+set VCPKG_ROOT=%~dp0vcpkg
+
+REM Инициализация vcpkg
+if not exist "%VCPKG_ROOT%\vcpkg.exe" (
+    echo Bootstrapping vcpkg...
+    call "%VCPKG_ROOT%\bootstrap-vcpkg.bat"
+)
+
+REM Установка пакетов
+echo Installing dependencies...
+"%VCPKG_ROOT%\vcpkg.exe" install curl jsoncpp
+
+REM Интеграция vcpkg с Visual Studio
+echo Integrating vcpkg with Visual Studio...
+"%VCPKG_ROOT%\vcpkg.exe" integrate install
+
+echo.
+echo vcpkg setup completed.
+echo.
+
+endlocal
+pause
